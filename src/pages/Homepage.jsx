857bdaa1aa296d5a6874b35ab3c9e16900f9ebc4
olayode demola <demolaolayode6@gmail.com>
2026-06-06 00:12:10 +0100

    added the header component to the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
-import NavBar from "../components/NavBar";
-
+import Header from "../components/Header";
 function Homepage() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <>
+      <Header />
+      <main></main>
+    </>
   );
 }
 
