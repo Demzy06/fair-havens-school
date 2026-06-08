c5a674e0ef14e7907dbf43ca205434c82b6cc590
olayode demola <demolaolayode6@gmail.com>
2026-06-08 15:15:00 +0100

    Added more component to file, and also made layout changes to the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,16 @@
+import AboutOurSchool from "../components/AboutOurSchool";
+import ActionCardsSection from "../components/ActionCardsSection";
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 function Homepage() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <HeroSection />
+        <ActionCardsSection />
+        <AboutOurSchool />
+      </main>
     </>
   );
 }
