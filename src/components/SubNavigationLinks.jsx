b7ec257567b55728a1459ee69a4a982d02c58c34
olayode demola <demolaolayode6@gmail.com>
2026-06-08 14:43:14 +0100

    Added components inside of the main tag, adding more to the layout

---
+++ src/components/SubNavigationLinks.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+function SubNavigationLinks({ navSubLinks }) {
+  console.log(navSubLinks);
+  return (
+    <ul className="mb-5">
+      {navSubLinks.map((navs, i) => (
+        <li className="mb-3 text-[14px] font-light">
+          <Link to={navSubLinks[i].toLowerCase().replaceAll(" ", "-")}>
+            {navs}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default SubNavigationLinks;
