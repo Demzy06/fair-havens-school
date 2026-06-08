97f122402a3205e58d333e0dacdf4c7d10ebbd42
olayode demola <demolaolayode6@gmail.com>
2026-06-08 15:02:42 +0100

    Created the logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/logo_.png";
+function Logo() {
+  return (
+    <div className="w-[2.5em] h-[2.5em]">
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default Logo;
