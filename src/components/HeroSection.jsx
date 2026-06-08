9e9092c6e1815107173f2dabf28505c8e3c2893a
olayode demola <demolaolayode6@gmail.com>
2026-06-08 15:15:38 +0100

    Created the HeroSection component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import Button from "./Button";
+
+function HeroSection() {
+  return (
+    <section>
+      <div className="text-center pt-22 pb-22 hey">
+        <h1 className="w-[80%] m-auto mb-6 uppercase zo">
+          Fair Havens Schools
+        </h1>
+        <p className="w-[80%] m-auto mb-6 font-medium text-white ">
+          Inspiring Excellence, Building Character, Shaping the Future
+        </p>
+        <Button
+          text="Learn More"
+          link="history"
+          className="text-black p-7 pt-2 pb-2 bg-white uppercase text-[13px]"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
