b15b44a7a98df26d5e83c8ab469fb408f531ded3
olayode demola <demolaolayode6@gmail.com>
2026-06-13 15:30:55 +0100

    Changed the height and width of the logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo_.png";
 function Logo() {
   return (
     <>
-      <img src={logo} className="w-[2.9em] h-[2.9em]" />
+      <img src={logo} className="w-[2.7em] h-[2.7em]" />
     </>
   );
 }
