a8eca270916607609f9284a2503ea2084e9acba4
olayode demola <demolaolayode6@gmail.com>
2026-06-14 23:28:48 +0100

    Built the Tuition page

--- src/pages/Tuition.jsx
+++ src/pages/Tuition.jsx
@@ -1,5 +1,34 @@
+import { title } from "framer-motion/client";
+import ContentSection from "../components/ContentSection";
+import SectionTitle from "../components/SectionTitle";
+import Footer from "../components/Footer";
+
+const tuitionDesc = {
+  title: "Ensuring your child’s education is supported by safe payment options",
+  description:
+    "We are dedicated to maintaining transparency and simplicity in all matters concerning fees and levies. Full details on charges and payments are accessible within the school. Our objective is to ensure a clear, smooth, and hassle-free payment process. Kindly find below the key information you need to know:",
+  items: [
+    "Payment Arrangement Notice: All financial obligations must be settled via bank transfer or cash on or before the commencement of each term. Any changes to fee amounts will be officially communicated to parents and guardians in advance",
+    "Fee Payment Options: Payments may be made annually or per term. All fees are payable before resumption and consist of Registration, Tuition, Deposit, Uniform and Caution components.",
+    "Registration Fee: A non-refundable registration fee is payable once to complete the admission process and secure your child’s placement in the school.",
+    "Caution Fee: A refundable caution fee is required upon enrollment. This fee is subject to the student’s compliance with school rules and regulations, as well as the absence of any damage to school property. It is payable at the point of admission.",
+  ],
+};
 function Tuition() {
-  return <div>Tuition</div>;
+  return (
+    <>
+      <section>
+        <SectionTitle text="School Fees" />
+        <ContentSection
+          title={tuitionDesc.title}
+          description={tuitionDesc.description}
+          items={tuitionDesc.items}
+          className="text-gray-200 bg-[linear-gradient(rgba(77,166,255,0.85),rgba(77,166,255,0.85)),url(/src/assets/logos-pic.jpg)] bg-contain bg-center bg-repeat"
+        />
+      </section>
+      <Footer />
+    </>
+  );
 }
 
 export default Tuition;
