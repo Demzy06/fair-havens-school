9585d23fe95fcf4672d641a3b706e035b12be51b
olayode demola <demolaolayode6@gmail.com>
2026-06-20 02:06:54 +0100

    feat: built this ovarlay and gallery slider component

---
+++ src/components/GallerySliderOverlay.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import ArrowBackIosRoundedIcon from "@iconify-react/material-symbols/arrow-back-ios-rounded";
+
+function GallerySliderOverlay({
+  onHandleOverlay,
+  clickedImage,
+  galleryImages,
+}) {
+  const [imageNum, setImageNum] = useState(clickedImage[0].id);
+  console.log(imageNum, galleryImages);
+
+  function increaseImgNum() {
+    if (imageNum === galleryImages.length) return;
+    setImageNum((imageNum) => imageNum + 1);
+    console.log(imageNum);
+  }
+  function decreaseImgNum() {
+    if (imageNum === 1) return;
+    setImageNum((imageNum) => imageNum - 1);
+    console.log(imageNum);
+  }
+
+  return (
+    <>
+      {/* Overlay */}
+      <div
+        className="fixed w-full h-full top-0 left-0 right-0 bottom-0 z-2 bg-[rgba(0,0,0,0.95)] overlay "
+        onClick={(e) => onHandleOverlay(e)}
+      >
+        {/* Image */}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[88%]  ">
+          <button
+            className="absolute -left-4.5 top-1/2 -translate-y-1/2 text-white z-100 text-2xl p-2.5 rounded-full bg-white/30 border-[#b8b8b8] border-[0.5px]"
+            onClick={() => decreaseImgNum()}
+          >
+            <img
+              src="/src/assets/icons/image.png"
+              className="w-[1.8rem] h-[1.8rem]"
+            />
+            {/* <ArrowBackIosRoundedIcon height="1em" /> */}
+          </button>
+          <img
+            src={galleryImages[imageNum - 1].src}
+            className="m-auto w-full z-10 rounded-2xl "
+          />
+          <button
+            className="absolute justify-center -right-4.5 top-1/2 -translate-y-1/2 text-white z-100 text-2xl transform-[rotate(180deg)] p-2.5 rounded-full bg-white/20 border-[#b8b8b8] border-[0.5px]"
+            onClick={() => increaseImgNum()}
+          >
+            <img
+              src="/src/assets/icons/image.png"
+              className="w-[1.8rem] h-[1.8rem]"
+            />
+            {/* <ArrowBackIosRoundedIcon height="1em"  /> */}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GallerySliderOverlay;
