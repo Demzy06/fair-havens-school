349ba6a1abfc9eab370f666d8acaaabcce4a4e36
olayode demola <demolaolayode6@gmail.com>
2026-06-21 01:02:53 +0100

    Restructured the codebase, made changes to styles

--- src/components/ActionCardsSection.jsx
+++ src/components/ActionCardsSection.jsx
@@ -1,12 +1,9 @@
 import ActionCard from "./ActionCard";
-import Button from "./Button";
 
 import FlyerImg from "../assets/image.png";
 import img1 from "../assets/excitedKids.jpeg";
 import img2 from "../assets/comp-lab-pic.jpeg";
 import img3 from "../assets/classroom-pic.jpeg";
-// import ClassroomImg from "../assets/classroom-pic.jpeg";
-import ClassroomImg from "../assets/building-3.jpg";
 const divsInfo = [
   {
     heading: "Schedule A Visit",
@@ -39,38 +36,11 @@ function ActionCardsSection() {
           <ActionCard info={info} key={info.heading} />
         ))}
       </div>
-      <div className="pr-2 pl-2 ">
-        <h3 className="text-center w-[95%] m-auto mt-10 mb-12 text-white">
+      <div className="pr-2 pl-2 pb-12 ">
+        <h3 className="text-center w-[95%] m-auto mt-10 mb-0 text-white">
           A place where every child belongs, learns, and thrives
         </h3>
       </div>
-
-      <section
-        className="pt-20 pb-20 pl-8 pr-8 text-white"
-        style={{
-          backgroundImage: `linear-gradient(rgba(15, 45, 82, 0.4)), url(${ClassroomImg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div>
-          <h2 className="mb-9">Building That Spark</h2>
-          <p className="">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
-            ab totam quasi reprehenderit, expedita maxime culpa dolorem, sunt
-            quidem, ex doloribus eaque incidunt nostrum vero magni! Suscipit ex
-            ad sunt repellat dolorem labore minima et consequuntur eveniet
-            soluta, architecto blanditiis rerum minus rem provident error. Harum
-            veritatis ab sed adipisci?
-          </p>
-          <Button
-            text="Apply Today"
-            className="bg-white  p-2 pl-5 pr-5 text-black mt-6 uppercase text-[13px]"
-            link="/contact"
-          />
-        </div>
-      </section>
     </section>
   );
 }
