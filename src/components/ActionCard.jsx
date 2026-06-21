3f00bcabbcd8b3b330232aae1df57dfc7dd65b93
olayode demola <demolaolayode6@gmail.com>
2026-06-21 01:01:21 +0100

    Made changes to the styles

--- src/components/ActionCard.jsx
+++ src/components/ActionCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 function ActionCard({ info }) {
   return (
     <Link to={info.linkTo}>
-      <div className="p-20 text-center mb-5 border-gray border-solid border-2 rounded-xl relative ">
+      <div className="p-26 text-center mb-5 border-gray border-solid border-2 rounded-xl relative ">
         {/* Background image */}
         <div
           className="absolute inset-0 bg-cover bg-center"
