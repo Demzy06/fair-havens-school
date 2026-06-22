ceaf6a6b347e8d78aa7051631107849c10433752
olayode demola <demolaolayode6@gmail.com>
2026-06-23 00:25:27 +0100

    feat: implemented fade in animation in the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,18 @@
 import Button from "./Button";
 import Header from "./Header";
+import { useInView } from "react-intersection-observer";
 
 function HeroSection() {
+  const { ref, inView } = useInView({ triggerOnce: true });
+
   return (
     <section className="hey">
       <Header />
-      <div className="text-center pt-22 pb-22 ">
+      <div
+        ref={ref}
+        className={`text-center pt-22 pb-22 transition-all duration-700 
+          ${inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
+      >
         <h1 className="w-[80%] m-auto mb-6 uppercase whiteColorShadow">
           Fair Havens Schools
         </h1>
