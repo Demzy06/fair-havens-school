5921031b71d6a95427873308467ddf607c61dcfd
olayode demola <demolaolayode6@gmail.com>
2026-06-25 14:08:49 +0100

    Made changes to the styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,16 +22,16 @@ function NavBar({ navIsOpen }) {
     <nav
       className={`h-screen md:h-fit bg-white ${navIsOpen ? "navAnimation" : ""} `}
     >
-      <ul className="text-center  text-[14px] w-[95%] m-auto font-[poppins] md:flex md:flex-row md:justify-evenly ">
+      <ul className="text-center text-[14px] w-[95%] m-auto font-[poppins] md:flex md:flex-row md:justify-evenly ">
         {subNavLinkName.map((name, i) => (
           <Nav navName={name.toUpperCase()} navSubLinks={allNavSubLinkArr[i]} />
         ))}
-        <li className="mb-4 w-[99%]">
+        <li className="mb-4 w-[92.7%]">
           <Link to="/gallery" className="uppercase ">
             Gallery
           </Link>
         </li>
-        <li className=" w-[98.5%]">
+        <li className=" w-[92.7%]">
           <Link to="/contact" className="uppercase">
             Contact
           </Link>
