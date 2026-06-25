f35a05857fef2c0cccba4926075949ef76381b8c
olayode demola <demolaolayode6@gmail.com>
2026-06-25 11:17:24 +0100

    feat: Implement animation on scroll

--- src/components/CallToActionSection.jsx
+++ src/components/CallToActionSection.jsx
@@ -1,7 +1,15 @@
 import Button from "./Button";
 import ClassroomImg from "../assets/building-3.jpg";
+import { useInView } from "react-intersection-observer";
 
 function CallToActionSection() {
+  const { ref: headingRef, inView: headingTextIsInVew } = useInView({
+    triggerOnce: true,
+  });
+  const { ref: paragraphRef, inView: paragraphTextIsInVew } = useInView({
+    triggerOnce: true,
+  });
+
   return (
     <section
       className="pt-20 pb-20 pl-8 pr-8 text-white"
@@ -13,18 +21,45 @@ function CallToActionSection() {
       }}
     >
       <div>
-        <h2 className="mb-9">Building That Spark</h2>
-        <p className="">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
-          ab totam quasi reprehenderit, expedita maxime culpa dolorem, sunt
-          quidem, ex doloribus eaque incidunt nostrum vero magni! Suscipit ex ad
-          sunt repellat dolorem labore minima et consequuntur eveniet soluta,
-          architecto blanditiis rerum minus rem provident error. Harum veritatis
-          ab sed adipisci?
+        <h2
+          ref={headingRef}
+          className={`mb-9 transition-all duration-700 ${
+            headingTextIsInVew
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-10"
+          }`}
+        >
+          Building That Spark
+        </h2>
+        <p
+          ref={paragraphRef}
+          className={`text-[15px] font-normal leading-6.5 transition-all duration-700 mb-2 ${
+            paragraphTextIsInVew
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-10"
+          }`}
+        >
+          We believe that within every child lies a unique spark waiting to be
+          discovered and nurtured. Through inspiring education, caring guidance,
+          and meaningful experiences, we ignite curiosity, foster creativity,
+          and cultivate a lifelong love for learning
+        </p>
+        <p
+          ref={paragraphRef}
+          className={`text-[15px] font-normal leading-6.5 transition-all duration-900 ${
+            paragraphTextIsInVew
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-10"
+          }`}
+        >
+          Our mission is to fan that spark into a bright flame of knowledge,
+          confidence, and purpose empowering our students to dream boldly, think
+          critically, and become compassionate leaders who will shape a better
+          future for their communities and the world.
         </p>
         <Button
           text="Apply Today"
-          className="bg-white p-2 pl-5 pr-5 text-black mt-6 uppercase text-[13px]"
+          className={`bg-white p-2 pl-5 pr-5 text-black mt-6 uppercase text-[13px]`}
           link="/contact"
         />
       </div>
