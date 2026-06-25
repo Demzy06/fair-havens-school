7355f996d7ba3fca759a9a6c7fc428b3d0e5f98d
olayode demola <demolaolayode6@gmail.com>
2026-06-25 11:15:49 +0100

    feat: Implement animation on scroll

--- src/components/SchoolStats.jsx
+++ src/components/SchoolStats.jsx
@@ -1,8 +1,17 @@
 import Button from "./Button";
 import Stats from "./Stats";
 import BuildingImg from "../assets/building-2.jpg";
+import { useInView } from "react-intersection-observer";
 
 function SchoolStats() {
+  const { ref: headingRef, inView: headingTextInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+  const { ref: paragraphRef, inView: paragraphTextInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
   return (
     <section className="relative pt-8 pb-2">
       {/* Background Image */}
@@ -21,11 +30,17 @@ function SchoolStats() {
 
       {/* Content */}
       <div className="pl-6 pr-6 text-white mb-15">
-        <h3 className="mb-7">
+        <h3
+          ref={headingRef}
+          className={`mb-7 transition-all duration-700 ${headingTextInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
+        >
           Building Tomorrow's Leaders Through Quality Education and Strong
           Values
         </h3>
-        <p className="mb-7">
+        <p
+          ref={paragraphRef}
+          className={`mb-7 transition-all duration-700 ${paragraphTextInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
+        >
           At Fair Havens School, we are dedicated to providing excellent
           education that nurtures character, discipline, and achievement. Learn
           more about our history, our caring community, and what makes us a
