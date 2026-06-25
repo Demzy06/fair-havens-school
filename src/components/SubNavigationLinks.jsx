dbd6e710e7348adf24682354e4cc0a3718b96080
olayode demola <demolaolayode6@gmail.com>
2026-06-25 14:05:29 +0100

    Made changes to the syles

--- src/components/SubNavigationLinks.jsx
+++ src/components/SubNavigationLinks.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 function SubNavigationLinks({ navSubLinks }) {
   console.log(navSubLinks);
   return (
-    <ul className="mb-5">
+    <ul className="mb-5 w-[92.7%]">
       {navSubLinks.map((navs, i) => (
         <li className="mb-3 text-[14px] font-light">
           <Link to={`/${navSubLinks[i].toLowerCase().replaceAll(" ", "-")}`}>
