dc760c86a610f9be0a35287af2fd2819775adf44
olayode demola <demolaolayode6@gmail.com>
2026-07-01 16:15:14 +0100

    Add sitemap and robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), Sitemap({ hostname: 'https://fairhavensschools.com.ng' })],
 })
