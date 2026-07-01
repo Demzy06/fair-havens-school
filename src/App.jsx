253640be43b74df8c5eae8575c4845b406646225
olayode demola <demolaolayode6@gmail.com>
2026-07-01 16:15:30 +0100

    comment vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router";
+// import { Analytics } from "@vercel/analytics/next";
+
 import Homepage from "./pages/Homepage";
 import History from "./pages/History";
 import FoundersNote from "./pages/FoundersNote";
@@ -19,36 +21,42 @@ import FairAtAGlance from "./pages/FairAtAGlance.jsx";
 import PrincipalNote from "./pages/PrincipalNote.jsx";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="fair-havens-at-a-glance" element={<FairAtAGlance />} />
-        <Route path="history" element={<History />} />
-        <Route path="founders-note" element={<FoundersNote />} />
-        <Route path="principal-note" element={<PrincipalNote />} />
-        <Route path="school-pledge-and-anthem" element={<SchoolAnthem />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="nursery-and-primary" element={<NurseryAndPrimary />} />
-        <Route path="secondary-and-highschool" element={<SecondarySchool />} />
-        <Route path="secondary-programs" element={<SecondaryPrograms />} />
-        <Route path="primary-programs" element={<PrimaryPrograms />} />
-        <Route
-          path="creche-and-nursery-programs"
-          element={<CrecheNurseryPrograms />}
-        />
-        <Route path="tuition" element={<Tuition />} />
-        <Route path="admission-process" element={<AdmissionProcess />} />
-        <Route
-          path="admission-exams-and-result"
-          element={<AdmissionExamsAndResult />}
-        />
-        <Route
-          path="co-curricular-activities-and-clubs"
-          element={<CoCurricular />}
-        />
-        <Route path="contact-us" element={<ContactUs />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {/* <Analytics /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="fair-havens-at-a-glance" element={<FairAtAGlance />} />
+          <Route path="history" element={<History />} />
+          <Route path="founders-note" element={<FoundersNote />} />
+          <Route path="principal-note" element={<PrincipalNote />} />
+          <Route path="school-pledge-and-anthem" element={<SchoolAnthem />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="nursery-and-primary" element={<NurseryAndPrimary />} />
+          <Route
+            path="secondary-and-highschool"
+            element={<SecondarySchool />}
+          />
+          <Route path="secondary-programs" element={<SecondaryPrograms />} />
+          <Route path="primary-programs" element={<PrimaryPrograms />} />
+          <Route
+            path="creche-and-nursery-programs"
+            element={<CrecheNurseryPrograms />}
+          />
+          <Route path="tuition" element={<Tuition />} />
+          <Route path="admission-process" element={<AdmissionProcess />} />
+          <Route
+            path="admission-exams-and-result"
+            element={<AdmissionExamsAndResult />}
+          />
+          <Route
+            path="co-curricular-activities-and-clubs"
+            element={<CoCurricular />}
+          />
+          <Route path="contact-us" element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
