f6c37788dc869209d0857f95353968cc5ecc9943
olayode demola <demolaolayode6@gmail.com>
2026-07-12 22:01:07 +0100

    made change to the layout for desktop responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,29 +7,39 @@ function Nav({ navName, navSubLinks, subNavClassName }) {
   function handleOpenSubNav() {
     setIsOpen(!isOpen);
   }
+
+  function handleCloseSubNav() {
+    setIsOpen(!isOpen);
+  }
   return (
-    <li className="mb-4">
-      <span className="flex justify-center mb-2">
-        <span className="m-auto w-fit">{navName}</span>
-        <button
-          className="cursor-pointer mr-1.5"
-          onClick={(e) => handleOpenSubNav(e)}
-        >
-          <img
-            src={ArrowDownIcon}
-            alt="arrow-down-icon"
-            className="w-5.5 h-5.5"
+    <>
+      <li
+        className="mb-4 md:m-0 md:relative md:pl-4 md:pr-4 md:p-2.5 md:font-medium md:text-[15px] cursor-pointer"
+        onMouseEnter={(e) => handleOpenSubNav(e)}
+        onMouseLeave={(e) => handleCloseSubNav(e)}
+      >
+        <span className="flex justify-center mb-2 md:m-0">
+          <span className="m-auto w-fit">{navName}</span>
+          <button
+            className="cursor-pointer mr-1.5 md:hidden"
+            onClick={(e) => handleOpenSubNav(e)}
+          >
+            <img
+              src={ArrowDownIcon}
+              alt="arrow-down-icon"
+              className="w-5.5 h-5.5"
+            />
+          </button>
+        </span>
+        {isOpen ? (
+          <SubNavigationLinks
+            navSubLinks={navSubLinks}
+            subNavClassName={subNavClassName}
+            subNavLinkName="about"
           />
-        </button>
-      </span>
-      {isOpen ? (
-        <SubNavigationLinks
-          navSubLinks={navSubLinks}
-          subNavClassName={subNavClassName}
-          subNavLinkName="about"
-        />
-      ) : null}
-    </li>
+        ) : null}
+      </li>
+    </>
   );
 }
 
