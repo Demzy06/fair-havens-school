77b544db7ecada110d9938e05236a8e9223ac6c5
olayode demola <demolaolayode6@gmail.com>
2026-07-12 21:52:12 +0100

    delete unused code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,25 +24,6 @@ const allNavSubLinkArr = [
 
 function NavBar({ navIsOpen, isSticky }) {
   return (
-    // <nav
-    //   className={`h-screen md:h-fit bg-white ${navIsOpen ? "navAnimation" : ""} `}
-    // >
-    //   <ul className="text-center text-[14px] w-[95%] m-auto font-[poppins] md:flex md:flex-row md:justify-evenly ">
-    //     {subNavLinkName.map((name, i) => (
-    //       <Nav navName={name.toUpperCase()} navSubLinks={allNavSubLinkArr[i]} />
-    //     ))}
-    //     <li className="mb-4 w-[92.7%]">
-    //       <Link to="/gallery" className="uppercase ">
-    //         Gallery
-    //       </Link>
-    //     </li>
-    //     <li className=" w-[92.6%]">
-    //       <Link to="/contact-us" className="uppercase">
-    //         Contact
-    //       </Link>
-    //     </li>
-    //   </ul>
-    // </nav>
     <nav
       className={`h-screen md:h-fit bg-white md:bg-transparent md:text-white ${navIsOpen ? "navAnimation" : ""} md:w-[55%] ${navIsOpen ? "block" : "hidden"} md:block`}
     >
