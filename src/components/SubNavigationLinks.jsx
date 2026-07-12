da629d33a8cc67e5430027a964cc63f9688fd1e6
olayode demola <demolaolayode6@gmail.com>
2026-07-12 21:46:46 +0100

    made change to the layout for desktop responsiveness

--- src/components/SubNavigationLinks.jsx
+++ src/components/SubNavigationLinks.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router";
 function SubNavigationLinks({ navSubLinks }) {
   console.log(navSubLinks);
   return (
-    <ul className="mb-5 w-[92.7%] md:w-[98%]">
+    <ul className="mb-5 w-[92.7%] md:w-50  md:pt-0 md:mb-0 md:absolute md:top-10 md:bg-white text-black md:text-left">
       {navSubLinks.map((navs, i) => (
-        <li className="mb-3 text-[14px] font-light">
+        <li className="mb-3 md:mb-0 text-[14px] font-light md:uppercase md:w-full md:hover:bg-gray-100 md:pl-4 md:pr-4 md:p-1">
           <Link to={`/${navSubLinks[i].toLowerCase().replaceAll(" ", "-")}`}>
             {navs}
           </Link>
